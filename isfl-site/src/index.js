cd96a35ab37e5bb98a63cabb5e82944aa06852cc
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-01 09:13:14 -0500

    Main JS file and accompanying CSS

---
+++ isfl-site/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import Router from './Router';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+          <Router />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
