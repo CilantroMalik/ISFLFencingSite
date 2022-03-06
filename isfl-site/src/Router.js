7e8920eeb17c86bc65af456c78218eb2ecf28501
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-06 18:26:57 -0500

    Import cleanup + add meet list route

--- isfl-site/src/Router.js
+++ isfl-site/src/Router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from "./features/home/Home";
 import { AddMeet } from "./features/addMeet/AddMeet";
+import { ListMeets } from "./features/meets/ListMeets";
 
 function Router() {
     return (
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/addMeet" element={<AddMeet />}/>
+            <Route path="/meets" element={<ListMeets />}/>
         </Routes>
     )
 }
