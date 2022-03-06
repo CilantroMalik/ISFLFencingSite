5e59c4091bbb32bc2f05493d10c6439c66e58150
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-06 18:28:14 -0500

    Begin working on detailed meet view

--- isfl-site/src/features/meets/ListMeets.js
+++ isfl-site/src/features/meets/ListMeets.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import React, { useEffect } from 'react'
 import { useNavigate } from "react-router";
 
--- isfl-site/src/features/meets/MeetDetail.js
+++ isfl-site/src/features/meets/MeetDetail.js
@@ -1,5 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from "react-router";
 
-export const MeetDetail = (props) => {
+export const MeetDetail = () => {
+    const location = useLocation()
+
+    const meetData = {}
+
+    useEffect(() => {
+        // fetch detailed meet data from API using location.state.id
+    })
+
+    return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+
+        </div>
+    )
 
 }
