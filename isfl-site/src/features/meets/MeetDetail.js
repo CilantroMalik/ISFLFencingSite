770af7630dae6f5d6fb133a132dbdd656f777504
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-08 17:57:30 -0500

    Add helper functions and begin detail view

--- isfl-site/src/features/meets/MeetDetail.js
+++ isfl-site/src/features/meets/MeetDetail.js
@@ -19,9 +19,33 @@ export const MeetDetail = () => {
         // fetch detailed meet data from API using location.state.id
     })
 
+    const toggleWeapon = (w) => {
+        if (weapon === w) { setWeapon("None") }
+        else { setWeapon(w) }
+    }
+
+    const exit = () => {
+        navigate("/")
+    }
+
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-
+            <h2>Choose a weapon to view results:</h2>
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                    <button style={{marginBottom: "1rem"}} onClick={() => toggleWeapon("Foil")} className={weapon === "Foil" ? "button" : "muted-button"}>Foil</button>
+                </div>
+                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                    <button onClick={() => toggleWeapon("Epee")} className={weapon === "Epee" ? "button" : "muted-button"} style={{marginLeft: "3rem", marginRight: "3rem", marginBottom: "1rem"}}>Epee</button>
+                </div>
+                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                    <button style={{marginBottom: "1rem"}} onClick={() => toggleWeapon("Saber")} className={weapon === "Saber" ? "button" : "muted-button"}>Saber</button>
+                </div>
+                <div style={{width: "2px", height: "5rem", marginLeft: "2rem", marginRight: "2rem", backgroundColor: "#f1f7ed", border: "1px solid #f1f7ed", borderRadius: "1px"}}> </div>
+                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}} >
+                    <button onClick={exit} style={{marginBottom: "1rem"}}>Exit</button>
+                </div>
+            </div>
         </div>
     )
 
---
+++ isfl-site/src/features/meets/SquadDetail.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const SquadDetail = (props) => {
+
+    return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", width: "40%"}}>
+            {props.squadData === undefined &&
+                <><h3>This squad didn't compete in this meet.</h3></>
+            }
+            { props.squadData !== undefined &&
+                <><h3 style={{color: "#f1f7ed"}}>{props.squadName}</h3>
+                <hr style={{width: "100%"}}/>
+                <table>
+                    <tr>
+                        <th style={{textAlign: "center"}}>Score 1</th>
+                        <th style={{textAlign: "center"}}>Fencer 1</th>
+                        <th style={{textAlign: "center"}}>Fencer 2</th>
+                        <th style={{textAlign: "center"}}>Score 2</th>
+                    </tr>
+                    {props.squadData.map(bout => (
+                        <tr>
+                            <td style={{textAlign: "center"}}>{bout.score1}</td>
+                            <td style={{textAlign: "center"}}>{bout.fencer1}</td>
+                            <td style={{textAlign: "center"}}>{bout.fencer2}</td>
+                            <td style={{textAlign: "center"}}>{bout.score2}</td>
+                        </tr>
+                    ))}
+                </table></>
+            }
+        </div>
+    )
+}
