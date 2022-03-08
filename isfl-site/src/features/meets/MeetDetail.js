eaec915c38aa8293b163e81f6cf81090a583e077
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-08 17:57:05 -0500

    Add imports and placeholder data to meet detail

--- isfl-site/src/features/meets/MeetDetail.js
+++ isfl-site/src/features/meets/MeetDetail.js
@@ -1,10 +1,19 @@
-import React, { useEffect } from 'react'
-import { useLocation } from "react-router";
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from "react-router";
+import { SquadDetail } from "../meets/SquadDetail";
 
 export const MeetDetail = () => {
     const location = useLocation()
+    const [weapon, setWeapon] = useState("None")
+    const navigate = useNavigate()
 
-    const meetData = {}
+    const meetData = {
+        squads: ["Boys Foil", "Girls Foil"],
+        squadData: {
+            "Boys Foil": [{fencer1: "Placeholder 1", fencer2: "Placeholder 2", score1: 5, score2: 3}],
+            "Girls Foil": [{fencer1: "Placeholder 3", fencer2: "Placeholder 4", score1: 4, score2: 5}],
+        }
+    }
 
     useEffect(() => {
         // fetch detailed meet data from API using location.state.id
