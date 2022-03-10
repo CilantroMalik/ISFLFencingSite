d292ff9439d0951981d3928403b854deeb9b0a33
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-10 07:42:07 -0500

    Helpers and scaffolding for team list

--- isfl-site/src/features/teams/ListTeams.js
+++ isfl-site/src/features/teams/ListTeams.js
@@ -1,5 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Header } from "../header/Header";
 
 export const ListTeams = () => {
+    const [weapon, setWeapon] = useState("None")
+    const [gender, setGender] = useState("Boys")
+    const teams = [
+        {id: "000000000", schoolName: "RCDS", squadName: "Boys Foil", wins: 5, losses: 4, boutsWon: 8, boutsLost: 6, touchesWon: 50, touchesLost: 35}
+    ]
+
+    useEffect(() => fetchTeams())
+
+    const toggleWeapon = (w) => {
+        if (weapon === w) { setWeapon("None") }
+        else { setWeapon(w) }
+    }
+
+    const fetchTeams = () => {
+        // Fetch team data from API and populate teams
+    }
+
+    const teamsForSquad = (squad) => {
+        let squadTeams = []
+        for (const team of teams) {
+            if (team.squadName === squad) {
+                squadTeams.push(team)
+            }
+        }
+        return squadTeams
+    }
 
 }
