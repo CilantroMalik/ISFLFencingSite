880d03ec43caea912494f04fd848614398b1be1c
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-09 19:43:23 -0500

    Minor modifications to team and squad detail views

--- isfl-site/src/features/meets/SquadDetail.js
+++ isfl-site/src/features/meets/SquadDetail.js
@@ -5,7 +5,10 @@ export const SquadDetail = (props) => {
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "center", width: "40%"}}>
             {props.squadData === undefined &&
-                <><h3>This squad didn't compete in this meet.</h3></>
+                <>
+                    <h3>{props.squadName}</h3>
+                    <h5>This squad didn't compete in this meet.</h5>
+                </>
             }
             { props.squadData !== undefined &&
                 <><h3 style={{color: "#f1f7ed"}}>{props.squadName}</h3>
