a689d9247ded60015c1c11916cd50c6f07b35caa
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-20 09:21:00 -0400

    Create helper functions

--- isfl-site/src/features/fencers/ListFencers.js
+++ isfl-site/src/features/fencers/ListFencers.js
@@ -11,4 +11,28 @@ export const ListFencers = () => {
 
     const fencers = [{id: "0000000", name: "Rohan Malik", school: "RCDS", boutsWon: 0, boutsLost: 1, touchesWon: 3, touchesLost: 5}]
 
+    const containsName = (name) => {
+        const names = name.split(" ")
+        for (const n of names) {
+            if (n.startsWith(searchFencer)) { return true }
+        }
+        return false
+    }
+
+    const fencersSearch = () => {
+        return fencers.filter(fencer => containsName(fencer.name)).filter(fencer => fencer.school.startsWith(searchSchool))
+    }
+
+    const clearClicked = () => {
+        setSearchFencer("")
+        setSearchSchool("")
+    }
+
+    return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+            <Header />
+            <h1>Fencers</h1>
+            <hr style={{width: "98%"}}/>
+        </div>
+    )
 }
