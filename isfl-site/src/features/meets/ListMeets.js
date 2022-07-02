ef9869ab46a75c01e64e84103e1b5e6ac643f556
Rohan Malik <rohan_malik@ryecountryday.org>
2022-07-02 16:49:05 -0400

    Improvements to meet UI and prep for fetching

--- isfl-site/src/features/meets/ListMeets.js
+++ isfl-site/src/features/meets/ListMeets.js
@@ -1,9 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router";
 import { Header } from "../header/Header";
+import { useSelector } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
 
 export const ListMeets = () => {
-    const meets = [{id: "0000000000", date: "02-26-2022", homeTeam: "RCDS", awayTeam: "Hackley", homeScore: "0", awayScore: "9"}]
+    // const meets = [{"id": 1646142830, "season": "20212022", "hteam": "Avenues", "ateam": "St Anns", "date": "2022-02-10",
+    //     "types": [
+    //         {"id": "1646142830ebv", "weapon": "epee", "gender": "boys", "level": "varsity", "hBW": "7", "aBW": "2", "hTW": "32", "aTW": "21"},
+    //         {"id": "1646142830fbv", "weapon": "foil", "gender": "boys", "level": "varsity", "hBW": "9", "aBW": "0", "hTW": "43", "aTW": "15"}
+    //     ]
+    // }]
+
+    const [meets, setMeets] = useState([])
 
     const navigate = useNavigate()
 
@@ -16,7 +25,11 @@ export const ListMeets = () => {
             <Header />
             <h1>Meets</h1>
             <hr style={{width: "98%"}}/>
-            <table style={{width: "60%"}}>
+            <table style={{width: "90%"}}>
+                <tr style={{fontSize: "1.7rem"}}>
+                    <th colSpan="3" style={{textAlign: "center"}}>Info</th>
+                    <th colSpan="6" style={{textAlign: "center"}}>Scores (Bouts/Touches)</th>
+                </tr>
                 <tr style={{fontSize: "1.7rem"}}>
                     <th>Date</th>
                     <th>Home Team</th>
